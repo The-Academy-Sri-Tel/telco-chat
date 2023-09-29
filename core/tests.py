653e59df8822296529f1eeb6cb115a182d35b40d
Sanduni <msandunim@gmail.com>
2023-09-29 23:49:28 +0530

    feat: telco chat with custom login

---
+++ core/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+# superuser
+# username: admin
+# email: admin@gmail.com
+# password: 1234
+# login User
+# username: hi
+# password - ?
